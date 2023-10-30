65b59217fb743881f58df5d2a7f8d966cc0ce031
qls051 <qls050100@naver.com>
2023-10-30 15:18:49 +0900

    CicrleHP 수정, 수류탄 탄창수 안 뜨는 버그 해결

--- Assets/01. Scripts/Player/CicrleHP.cs
+++ Assets/01. Scripts/Player/CicrleHP.cs
@@ -18,7 +18,7 @@ public class CicrleHP : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.U))
         {
             DecreaseHP(10);
         }
