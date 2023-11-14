e006488079f43146aa730954e08a02417a0f8b41
Leeyebin <qls050100@naver.com>
2023-11-14 11:47:49 +0900

    Login cs 작성
    근데 게임 시작하고 로그인 씬으로 이동하면 inputfield와 buttn 클릭이 안되는 버그 발생

--- Assets/01. Scripts/Server/Login.cs
+++ Assets/01. Scripts/Server/Login.cs
@@ -6,9 +6,9 @@ using BackEnd;
 public class Login : LoginBase
 {
     [SerializeField] private Image imageID; // ID 필드 색상 변경
-    [SerializeField] private Text inputFieldID; // ID 필드 텍스트 정보 추출
+    [SerializeField] private InputField inputFieldID; // ID 필드 텍스트 정보 추출
     [SerializeField] private Image imagePW; // PW 필드 색상 변경
-    [SerializeField] private Text inputFieldPW; // PW 필드 텍스트 정보 추출
+    [SerializeField] private InputField inputFieldPW; // PW 필드 텍스트 정보 추출
 
     [SerializeField] private Button btnLogin; // 로그인 버튼 (상호작용 가능/불가능)
 
@@ -25,5 +25,78 @@ public class Login : LoginBase
         if (IsFieldDataEmpty(imagePW, inputFieldPW.text, "비밀번호")) return;
 
         // 로그인 버튼 연타하지 못하도록 상호작용 비활성화
+        btnLogin.interactable = false;
+
+        // 서버에 로그인을 요청하는 동안 화면에 출력하는 내용 업데이트
+        // ex) 로그인 관련 텍스트 출력, 톱니바퀴 아이콘 회전 등
+        StartCoroutine(nameof(LoginProcess));
+
+        // 뒤끝서버 로그인 시도
+        ResponseToLogin(inputFieldID.text, inputFieldPW.text);
+    }
+
+    /// <summary>
+    /// 로그인 시도 후 서버로부터 전달받은 message를 기반으로 로직 처리
+    /// </summary>
+    private void ResponseToLogin(string ID, string PW)
+    {
+        // 서버에 로그인 요청
+        Backend.BMember.CustomLogin(ID, PW, callback =>
+        {
+            StopCoroutine(nameof(LoginProcess));
+
+            // 로그인 성공
+            if (callback.IsSuccess())
+            {
+                SetMessage($"{inputFieldID.text}님 환영합니다!");
+            }
+            // 로그인 실패
+            else
+            {
+                // 로그인에 실패했을 때 다시 로그인을 해야하기 때문에 "로그인"버튼 상호작용 활성화
+                btnLogin.interactable = true;
+
+                string message = string.Empty;
+
+                switch (int.Parse(callback.GetStatusCode()))
+                {
+                    case 401: // 존재하지 않는 아이디, 잘못된 비밀번호
+                        message = callback.GetMessage().Contains("customID") ? "존재하지 않는 아이디입니다." : "잘못된 비밀번호 입니다.";
+                        break;
+                    case 403: // 유저 or 디바이스 차단
+                        message = callback.GetMessage().Contains("user") ? "차단당한 유저입니다." : "차단당한 디바이스 입니다.";
+                        break;
+                    case 410: // 탈퇴 진행중
+                        message = "탈퇴가 진행중인 유저입니다.";
+                        break;
+                    default:
+                        message = callback.GetMessage();
+                        break;
+                }
+
+                // StatusCode 401에서 "잘못된 비밀번호 입니다." 일떄
+                if (message.Contains("비밀번호"))
+                {
+                    GuideForIncorrectlyEnteredData(imagePW, message);
+                }
+                else
+                {
+                    GuideForIncorrectlyEnteredData(imageID, message);
+                }
+            }
+        });
+    }
+
+    private IEnumerator LoginProcess()
+    {
+        float time = 0;
+        while (true)
+        {
+            time += Time.deltaTime;
+
+            SetMessage($"로그인 중입니다...{time:F1}");
+
+            yield return null;
+        }
     }
 }
