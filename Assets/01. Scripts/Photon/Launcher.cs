179dca4dae6afcbf71fa40624276d1e3df7b9e0f
Leeyebin <qls050100@naver.com>
2023-11-19 15:05:50 +0900

    Launcher cs 툴 작성, 버튼 디자인, MenuManager cs 생성
    방 만들기, 방 찾기, Quit Game 버튼 만듦

--- Assets/01. Scripts/Photon/Launcher.cs
+++ Assets/01. Scripts/Photon/Launcher.cs
@@ -6,6 +6,18 @@ public class Launcher : MonoBehaviourPunCallbacks
 {
     private void Start()
     {
+        Debug.Log("Conneting to Master");
         PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
     }
 }
