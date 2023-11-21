abd5f446da62f62e004a03babf5d2b8315444d40
Leeyebin <qls050100@naver.com>
2023-11-21 11:45:20 +0900

    빌드 하다 에러 나서 몇몇 cs 수정
    enemyProjectile cs using UnityEditor.PackageManager; <= 주석처리
    
    enemyMemoryPool cs using UnityEngine.UIElements;이거 두개 라서 하나 삭제
    
    MemoryPool cs using UnityEditor.ShaderKeywordFilter; <= 주석처리

--- Assets/01. Scripts/ObjectPooling/MemoryPool.cs
+++ Assets/01. Scripts/ObjectPooling/MemoryPool.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEditor.ShaderKeywordFilter;
+//using UnityEditor.ShaderKeywordFilter;
 
 public class MemoryPool
 {
