fe7eee7e3d374359cc263737c303b86553892e61
Leeyebin <qls050100@naver.com>
2023-11-23 23:50:53 +0900

    RoomManager cs 작성
    Roommanager에 RoomManager cs 넣고 PlayerManager 만들고 프리팹으로 만든 후 Photonview cs 넣어줌

---
+++ Assets/01. Scripts/Photon/RoomManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager Instance;
+
+    private void Awake()
+    {
+        if (Instance) // Checks if another RoomManager exisis   
+        {
+            Destroy(gameObject); // there can only be one
+            return;
+        }
+        DontDestroyOnLoad(gameObject); // I am the only one
+        Instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if(scene.buildIndex == 1) // build index 값 나중에 최종 빌드때 2로 바꿔야함 
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
+        }
+    }
+}
