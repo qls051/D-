7a25af54163cf41b916e2c6504aaa8de2ea562f9
Leeyebin <qls050100@naver.com>
2023-11-25 19:49:54 +0900

    SpawnPoint cs 작성

--- Assets/01. Scripts/Photon/SpawnPoint.cs
+++ Assets/01. Scripts/Photon/SpawnPoint.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject graphics;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        graphics.SetActive(false);
     }
 }
